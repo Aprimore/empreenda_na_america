46ddc054d978952b39304325490f684859b2bb24
Gabriel Christo <gabrielchristomarques@gmail.com>
2024-09-06 10:35:53 -0300

    test1

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -6,16 +6,16 @@ export const trailingSlash = 'always';
 export const prerender = true;
 export const ssr = true;
 
-export async function load({ params, url }) {
-	if (browser) {
-		const lang = params.lang || 'en'; // Fallback to 'en' if lang is not provided
+// export async function load({ params, url }) {
+// 	if (browser) {
+// 		const lang = params.lang || 'en'; // Fallback to 'en' if lang is not provided
 
-		if (!params.lang) {
-			throw redirect(307, `/${lang}${url.pathname}`);
-		}
+// 		if (!params.lang) {
+// 			throw redirect(307, `/${lang}${url.pathname}`);
+// 		}
 
-		return {
-			lang
-		};
-	}
-}
+// 		return {
+// 			lang
+// 		};
+// 	}
+// }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,43 +4,44 @@
 	import { t } from '$lib/translations';
 	import { onMount } from 'svelte';
 
-	let loading = true;
+	// let loading = true;
 
-	onMount(() => {
-		loading = false;
-	});
+	// onMount(() => {
+	// 	loading = false;
+	// });
 </script>
 
-{#if loading}
+<!-- {#if loading}
 	<h1>Loading...</h1>
-{:else}
-	<section class="w-full max-sm:p-2 bg-[#dfdad6] text-balance h-screen">
-		<div class="lg:max-w-screen-2xl mx-auto max-sm:p-0">
-			<div class="lg:p-10 sm:p-5 flex max-md:flex-col items-center">
-				<div class="md:max-w-[50%] m-auto border-black align-middle">
-					<h1 class="font-extrabold text-4xl md:text-5xl xl:text-7xl uppercase">
-						Entrepreneur in America: Your Complete Map to Success in the USA
-					</h1>
-					<h2 class="font-bold text-xl lg:text-2xl lg:mt-10 md:mt-5 mt-3">
-						Discover exclusive strategies for business, investment, and financial education to grow
-						in the United States.
-					</h2>
-				</div>
-				<div class="relative w-full">
-					<!-- Placeholder while loading -->
-					<div class="skeleton-loader w-full h-full"></div>
-					<enhanced:img
-						style="width:100%; height:100%; object-fit:cover"
-						src={headerhero_webp}
-						alt="landing page header"
-						loading="lazy"
-						sizes="(min-width:1080px) 744px, (min-width:768px) 640px, 400px"
-					/>
-				</div>
+{:else} -->
+<section class="w-full max-sm:p-2 bg-[#dfdad6] text-balance h-screen">
+	<div class="lg:max-w-screen-2xl mx-auto max-sm:p-0">
+		<div class="lg:p-10 sm:p-5 flex max-md:flex-col items-center">
+			<div class="md:max-w-[50%] m-auto border-black align-middle">
+				<h1 class="font-extrabold text-4xl md:text-5xl xl:text-7xl uppercase">
+					Entrepreneur in America: Your Complete Map to Success in the USA
+				</h1>
+				<h2 class="font-bold text-xl lg:text-2xl lg:mt-10 md:mt-5 mt-3">
+					Discover exclusive strategies for business, investment, and financial education to grow in
+					the United States.
+				</h2>
+			</div>
+			<div class="relative w-full">
+				<!-- Placeholder while loading -->
+				<div class="skeleton-loader w-full h-full"></div>
+				<enhanced:img
+					style="width:100%; height:100%; object-fit:cover"
+					src={headerhero_webp}
+					alt="landing page header"
+					loading="lazy"
+					sizes="(min-width:1080px) 744px, (min-width:768px) 640px, 400px"
+				/>
 			</div>
 		</div>
-	</section>
-{/if}
+	</div>
+</section>
+
+<!-- {/if} -->
 
 <style>
 	* {
