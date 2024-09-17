974076410214b25c4106d554a3e6f983e25cc5b2
Gabriel Christo <gabrielchristomarques@gmail.com>
2024-09-17 13:42:35 -0300

    css nano test

--- src/routes/[lang]/+layout.svelte
+++ src/routes/[lang]/+layout.svelte
@@ -29,18 +29,6 @@
 <!-- <svelte:head>
 	<title>{getPageTitle(pagePath)}</title>
 	<link rel="canonical" href={SITE_URL + pagePath} />
-	<meta property="og:url" content={SITE_URL} />
-	<meta property="og:type" content="article" />
-	<meta property="og:title" content={SITE_TITLE} />
-	<meta name="description" content={SITE_DESCRIPTION} />
-	<meta property="og:description" content={SITE_DESCRIPTION} />
-	<meta property="og:image" content={toptier_webp} />
-	<meta property="og:image:width" content={OG_IMAGE_WIDTH} />
-	<meta property="og:image:height" content={OG_IMAGE_HEIGHT} />
-	<meta name="twitter:image" content={toptier_webp} />
-	<meta name="twitter:card" content="summary_large_image" />
-	<meta name="twitter:title" content={SITE_TITLE} />
-	<meta name="twitter:description" content={SITE_DESCRIPTION} />
 </svelte:head> -->
 
 <!-- <ViewTransition /> -->
@@ -66,42 +54,5 @@
 		:global(html) {
 			zoom: 1;
 		}
-	} */
-
-	/* :global(html) {
-		font-family: 'Inter Variable';
-		font-weight: 700;
-		font-optical-sizing: auto;
-		font-style: normal;
-		font-display: swap;
-	}
-	:global(.Lora) {
-		font-family: 'Lora Variable', serif, Arial;
-		font-weight: 400;
-		font-optical-sizing: auto;
-		font-style: normal;
-		font-display: swap;
-	}
-	:global(.Lora-Bold) {
-		font-family: 'Lora Variable', serif, Arial;
-		font-weight: 700;
-		font-optical-sizing: auto;
-		font-style: normal;
-		font-display: swap;
-	}
-	:global(.Inter) {
-		font-family: 'Inter Variable', sans-serif, Arial;
-		font-weight: 400;
-		font-optical-sizing: auto;
-		font-style: normal;
-		font-display: swap;
-	}
-
-	:global(.Inter-Bold) {
-		font-family: 'Inter Variable', sans-serif, Arial;
-		font-weight: 700;
-		font-optical-sizing: auto;
-		font-style: normal;
-		font-display: swap;
 	} */
 </style>
