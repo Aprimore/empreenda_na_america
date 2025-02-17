1d83c4bb407b343d31e3ed622e5a6741feb5f1fc
Gabriel Christo <gabrielchristomarques@gmail.com>
2025-02-17 18:20:59 -0300

    quickfix FB Pixel

--- src/lib/loadGTM.ts
+++ src/lib/loadGTM.ts
@@ -35,55 +35,6 @@ export function loadGA() {
 	});
 }
 
-// export function loadFacebookPixel() {
-// 	return new Promise((resolve, reject) => {
-// 		try {
-// 			// Fetch para pegar o Pixel ID do arquivo PHP
-// 			fetch('https://empreendanaamerica.com/v2/facebook-conversion.php')
-// 				.then((response) => response.json())
-// 				.then((data) => {
-// 					console.log(data, response);
-// 					const pixelId = data.pixel_id; // Atribui o Pixel ID retornado pela API
-
-// 					if (!pixelId) {
-// 						throw new Error('Pixel ID não encontrado');
-// 					}
-
-// 					// Inicializa o Pixel com o ID obtido
-// 					(function (f, b, e, v, n, t, s) {
-// 						if (f.fbq) return;
-// 						n = f.fbq = function () {
-// 							n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
-// 						};
-// 						if (!f._fbq) f._fbq = n;
-// 						n.push = n;
-// 						n.loaded = !0;
-// 						n.version = '2.0';
-// 						n.queue = [];
-// 						t = b.createElement(e);
-// 						t.async = !0;
-// 						t.src = v;
-// 						s = b.getElementsByTagName(e)[0];
-// 						s.parentNode.insertBefore(t, s);
-// 					})(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
-
-// 					fbq('init', pixelId); // Usando o Pixel ID obtido dinamicamente
-// 					fbq('track', 'PageView'); // Rastreia a visualização da página
-
-// 					console.log('Facebook Pixel carregado com sucesso'); // Verifica se deu certo
-// 					resolve();
-// 				})
-// 				.catch((error) => {
-// 					console.error('Erro ao carregar o Pixel ID:', error);
-// 					reject(error);
-// 				});
-// 		} catch (error) {
-// 			console.error('Erro ao carregar o Facebook Pixel:', error);
-// 			reject(error);
-// 		}
-// 	});
-// }
-
 export async function sendFacebookPixelEvent() {
 	try {
 		const response = await fetch('https://empreendanaamerica.com/v2/facebook-conversion.php/', {
@@ -104,11 +55,12 @@ export async function sendFacebookPixelEvent() {
 			})
 		});
 
-		console.log(response);
+		// console.log(response);
 
 		if (response.ok) {
-			const data = await response.json();
-			console.log('Evento enviado:', data);
+			// const data =
+			await response.json();
+			// console.log('Evento enviado:', data);
 		} else {
 			console.error('Erro ao fazer o fetch:', response.statusText);
 		}
